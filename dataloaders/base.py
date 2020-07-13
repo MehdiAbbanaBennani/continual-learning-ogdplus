de1df2ca235bdce829260ebbc11edd26aa730a68
Mehdi Abbana Bennani <mehdi.abbana.bennani+github@gmail.com>
2020-07-13 02:13:47 +0200

    Add Licence and required files from Continual-Learning-Benchmark repo

---
+++ dataloaders/base.py
@@ -0,0 +1,139 @@
+import torchvision
+from torchvision import transforms
+from .wrapper import CacheClassLabel
+import torchvision.transforms.functional as TF
+
+
+class RotationTransform:
+    """Rotate by one of the given angles."""
+
+    def __init__(self, angle):
+        self.angle = angle
+
+    def __call__(self, x):
+        return TF.rotate(x, self.angle)
+
+
+def MNIST(dataroot, train_aug=False, angle=0):
+    # Add padding to make 32x32
+    #normalize = transforms.Normalize(mean=(0.1307,), std=(0.3081,))  # for 28x28
+    normalize = transforms.Normalize(mean=(0.1000,), std=(0.2752,))  # for 32x32
+    rotate = RotationTransform(angle=angle)
+
+    val_transform = transforms.Compose([
+        transforms.Pad(2, fill=0, padding_mode='constant'),
+        rotate,
+        transforms.ToTensor(),
+        normalize,
+    ])
+    train_transform = val_transform
+    if train_aug:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            rotate,
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+    train_dataset = torchvision.datasets.MNIST(
+        root=dataroot,
+        train=True,
+        download=True,
+        transform=train_transform
+    )
+    train_dataset = CacheClassLabel(train_dataset)
+
+    val_dataset = torchvision.datasets.MNIST(
+        dataroot,
+        train=False,
+        transform=val_transform
+    )
+    val_dataset = CacheClassLabel(val_dataset)
+
+    return train_dataset, val_dataset
+
+
+# def ToyMNIST(dataroot, train_aug=False, count=10) :
+#     train_dataset, val_dataset = MNIST(dataroot, train_aug)
+#
+#     train_dataset.dataset.data = train_dataset.dataset.data[:count]
+#     train_dataset.labels = train_dataset.labels[:count]
+#
+#     val_dataset.dataset.data = val_dataset.dataset.data[:count]
+#     val_dataset.labels = val_dataset.labels[:count]
+#
+#     return train_dataset, val_dataset
+# TODO : Complete this section
+
+
+def CIFAR10(dataroot, train_aug=False, angle=0):
+    normalize = transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262])
+    rotate = RotationTransform(angle=angle)
+
+    val_transform = transforms.Compose([
+        rotate,
+        transforms.ToTensor(),
+        normalize,
+    ])
+    train_transform = val_transform
+    if train_aug:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            rotate,
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+    train_dataset = torchvision.datasets.CIFAR10(
+        root=dataroot,
+        train=True,
+        download=True,
+        transform=train_transform
+        )
+    train_dataset = CacheClassLabel(train_dataset)
+
+    val_dataset = torchvision.datasets.CIFAR10(
+        root=dataroot,
+        train=False,
+        download=True,
+        transform=val_transform
+    )
+    val_dataset = CacheClassLabel(val_dataset)
+
+    return train_dataset, val_dataset
+
+
+def CIFAR100(dataroot, train_aug=False, angle=0):
+    normalize = transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
+
+    val_transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
+    train_transform = val_transform
+    if train_aug:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+    train_dataset = torchvision.datasets.CIFAR100(
+        root=dataroot,
+        train=True,
+        download=True,
+        transform=train_transform
+    )
+    train_dataset = CacheClassLabel(train_dataset)
+
+    val_dataset = torchvision.datasets.CIFAR100(
+        root=dataroot,
+        train=False,
+        download=True,
+        transform=val_transform
+    )
+    val_dataset = CacheClassLabel(val_dataset)
+
+    return train_dataset, val_dataset
